c782372b893e9bf0336f7901cbf9b07be6d86bd1
tyree <tyreepearson88@gmail.com>
2022-07-29 13:54:56 -0500

    added terraform files

---
+++ main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+        random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  cloud {
+    organization = "tyreepearson"
+    workspaces {
+      
+      name = "standard-demo"
+    }
+  }
+
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "module" {
+  source  = "app.terraform.io/tyreepearson/module/aws"
+  name    = var.name
+  region  = var.region
+  prefix  = var.prefix
+  version = "1.0.0"
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type =var.instance_size
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+module "s3-webapp" {
+  source  = "app.terraform.io/tyreepearson/s3-webapp/aws"
+  name    = var.name
+  region  = var.region
+  prefix  = var.prefix
+  version = "1.0.0"
+}
+module "ecs" {
+  source  = "terraform-aws-modules/ecs/aws"
+  version = "3.4.1"
+  name = "ecs-module-7"
+  # insert required variables here
+}
---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "instance_size" {
+  description = "instance type"
+}
+variable "prefix" {
+  description ="environment prefex"
+}
+variable "name" {
+  description ="name of owner"
+}
+variable "region" {
+  description = "aws region"
+}
