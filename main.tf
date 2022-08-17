4307fceba220626a678d36dd38a0745127acac1d
tyree <tyreepearson88@gmail.com>
2022-08-17 16:12:27 -0500

    changed ec2

--- main.tf
+++ main.tf
@@ -63,6 +63,6 @@ module "s3-webapp" {
 module "ecs" {
   source  = "terraform-aws-modules/ecs/aws"
   version = "3.4.1"
-  name = "ecs-module-3"
+  name = "ecs-module-4"
   # insert required variables here
 }
